bac632d823498a70410c9c5ede1625c3c148ba85
merytpeters <merytpeters@gmail.com>
2024-10-31 19:30:42 +0100

    UTF-8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,24 +6,21 @@ def validUTF8(data):
     """Checks that the data represents a valid UTF-8 encoding"""
     num_bytes = 0
 
-    mask1 = 1 << 7
-    mask2 = 1 << 6
-
     for byte in data:
-        byte = byte & 0xFF
-
         if num_bytes == 0:
-            if (byte >> 5) == 0b110:
+            if (byte >> 7) == 0:
+                num_bytes == 0
+            elif (byte >> 5) == 0b110:
                 num_bytes = 1
             elif (byte >> 4) == 0b1110:
                 num_bytes = 2
             elif (byte >> 3) == 0b11110:
                 num_bytes = 3
-            elif (byte >> 7) == 1:
-                return False
             else:
-                if not (byte & mask1 and not (byte & mask2)):
-                    return True
-                num_bytes -= 1
+                return False
+        else:
+            if byte >> 6 != 0b10:
+                return False
+            num_bytes -= 1
 
     return num_bytes == 0
