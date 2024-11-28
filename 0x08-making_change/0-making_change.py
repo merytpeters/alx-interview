331075826a271701d486d909449a66e440e87ffc
merytpeters <merytpeters@gmail.com>
2024-11-28 14:21:33 +0100

    Improving efficiency of code

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -9,11 +9,13 @@ def makeChange(coins, total):
 
     changeCount = 0
     coins.sort(reverse=True)
-    for coin in coins:
-        while total >= coin:
-            total -= coin
-            changeCount += 1
 
+    for coin in coins:
         if total == 0:
-            return changeCount
-    return -1
+            break
+        # divide total by the coin to see how many of that coin is needed
+        changeCount += total // coin
+        # append the remainder to get new total
+        total %= coin
+    # check if new total is 0
+    return changeCount if total == 0 else -1
